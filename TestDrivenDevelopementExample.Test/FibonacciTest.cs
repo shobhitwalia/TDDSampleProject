0b2fd0c003aec0c28d9fd2eea6c854b0e824a692
shobhit walia <shobhit.walia@quovantis.com>
2018-03-22 11:00:00 +0530

    Initial Commit

---
+++ TestDrivenDevelopementExample.Test/FibonacciTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopementExample;
+
+namespace TestDrivenDevelopementExample.Test
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        [TestMethod]
+        public void GetFibonacciNumber_Given0_Return0()
+        {
+            //arrange
+            int number = 0;
+            
+            //act
+            int result = Fibonacci.GetFibonacciNumber(number);
+
+            //assert
+            Assert.AreEqual(0, result); 
+        }
+
+        [TestMethod]
+        public void GetFibonacciNumber_Given1_Return1()
+        {
+            //arrange
+            int number = 1;
+
+            //act
+            int result = Fibonacci.GetFibonacciNumber(number);
+
+            //assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void GetFibonacciNumber_Given2_Return1()
+        {
+            //arrange
+            int number = 2;
+
+            //act
+            int result = Fibonacci.GetFibonacciNumber(number);
+
+            //assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void GetFibonacciNumber_Given3_Return2()
+        {
+            //arrange
+            int number = 3;
+
+            //act
+            int result = Fibonacci.GetFibonacciNumber(number);
+
+            //assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GetFibonacciNumber_Given4_Return3()
+        {
+            //arrange
+            int number = 4;
+
+            //act
+            int result = Fibonacci.GetFibonacciNumber(number);
+
+            //assert
+            Assert.AreEqual(3, result);
+        }
+
+    }
+}
---
+++ TestDrivenDevelopementExample/Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopementExample
+{
+    public class Fibonacci
+    {
+        public static int GetFibonacciNumber(int number)
+        {
+            /*
+            Step 1 : 
+            return 0;
+
+            Step 2:
+            if (number == 0)
+                return 0;
+            else 
+                return 1;
+
+            Step 3:
+          
+            if (number == 0)
+                return 0;
+            else if (number <= 2)
+                return 1;
+            else 
+                return 2;
+             */
+
+            if (number == 0)
+                return 0;
+            else if (number <= 2)
+                return 1;
+            else
+                return GetFibonacciNumber(number-1) + GetFibonacciNumber(number -2);
+        }
+    }
+}
